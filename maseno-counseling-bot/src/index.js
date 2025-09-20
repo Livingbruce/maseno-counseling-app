19009c8ba655fb1028c48178a73b0d7b9e7f7e0b
Livingbruce <vicymbrush@gmail.com>
2025-09-20 18:41:01 +0300

    Fix CORS configuration for Netlify frontend connection

--- maseno-counseling-bot/src/index.js
+++ maseno-counseling-bot/src/index.js
@@ -58,7 +58,11 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: true, // Allow all origins for now
+  origin: [
+    'https://maseno-counseling-bot.netlify.app',
+    'http://localhost:3000',
+    'http://localhost:5173'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
